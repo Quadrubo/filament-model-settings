8c15b619e0367d6e7e2203f43e3d86fea8645b25
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2024-03-17 14:28:21 +0100

    feat: cluster support (#13)

--- src/Commands/MakeModelSettingsPageCommand.php
+++ src/Commands/MakeModelSettingsPageCommand.php
@@ -2,8 +2,10 @@
 
 namespace Quadrubo\FilamentModelSettings\Commands;
 
+use Filament\Clusters\Cluster;
 use Filament\Facades\Filament;
 use Filament\Panel;
+use Filament\Support\Commands\Concerns\CanIndentStrings;
 use Filament\Support\Commands\Concerns\CanManipulateFiles;
 use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
@@ -13,6 +15,7 @@ use function Laravel\Prompts\text;
 
 class MakeModelSettingsPageCommand extends Command
 {
+    use CanIndentStrings;
     use CanManipulateFiles;
 
     protected $description = 'Create a new Filament model settings page class';
@@ -80,8 +83,22 @@ class MakeModelSettingsPageCommand extends Command
             return static::INVALID;
         }
 
+        $potentialCluster = (string) str($namespace)->beforeLast('\Pages');
+        $clusterAssignment = null;
+        $clusterImport = null;
+
+        if (
+            class_exists($potentialCluster) &&
+            is_subclass_of($potentialCluster, Cluster::class)
+        ) {
+            $clusterAssignment = $this->indentString(PHP_EOL . PHP_EOL . 'protected static ?string $cluster = ' . class_basename($potentialCluster) . '::class;');
+            $clusterImport = "use {$potentialCluster};" . PHP_EOL;
+        }
+
         $this->copyStubToApp('ModelSettingsPage', $path, [
             'class' => $pageClass,
+            'clusterAssignment' => $clusterAssignment,
+            'clusterImport' => $clusterImport,
             /** @phpstan-ignore-next-line */
             'namespace' => str($namespace ?? '') . ($pageNamespace !== '' ? "\\{$pageNamespace}" : ''),
         ]);
