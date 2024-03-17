b3c60e572e346ad3c682d217fced3c0ed7c24119
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2024-03-17 14:10:03 +0100

    fix: database transactions stuff too early

--- src/Pages/ModelSettingsPage.php
+++ src/Pages/ModelSettingsPage.php
@@ -8,17 +8,14 @@ use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Pages\Concerns;
-use Filament\Pages\Page;
 use Filament\Support\Exceptions\Halt;
 use Quadrubo\FilamentModelSettings\Exceptions\HasModelSettingsNotImplementedException;
 use Quadrubo\FilamentModelSettings\Pages\Contracts\HasModelSettings;
 use Illuminate\Support\Facades\DB;
-use Throwable;
 use Filament\Pages\Concerns\CanUseDatabaseTransactions;
 
 class ModelSettingsPage extends Page implements HasForms
 {
-    use CanUseDatabaseTransactions;
     use Concerns\InteractsWithFormActions;
 
     protected static string $view = 'filament-model-settings::pages.model-settings-page';
@@ -67,8 +64,6 @@ class ModelSettingsPage extends Page implements HasForms
         }
 
         try {
-            $this->beginDatabaseTransaction();
-
             $this->callHook('beforeValidate');
 
             /** @phpstan-ignore-next-line */
@@ -85,18 +80,8 @@ class ModelSettingsPage extends Page implements HasForms
             $settings->apply((array) $data);
 
             $this->callHook('afterSave');
-
-            $this->commitDatabaseTransaction();
         }  catch (Halt $exception) {
-            $exception->shouldRollbackDatabaseTransaction() ?
-                $this->rollBackDatabaseTransaction() :
-                $this->commitDatabaseTransaction();
-
             return;
-        } catch (Throwable $exception) {
-            $this->rollBackDatabaseTransaction();
-
-            throw $exception;
         }
 
         $this->getSavedNotification()?->send();
